77487d6523a6fdc7b718a3b4e6237a6ee6ed58fd
amirtavass <amirtavass62@gmail.com>
2025-11-12 11:40:53 +0000

    color changed again

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ function AppLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 bg-slate-300">{children}</main>
+      <main className="flex-1 bg-purple-300">{children}</main>
       <Footer />
     </div>
   );
